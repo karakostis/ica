16822418422df0d07f3afbba0cccab1f1bf90137
karakostis <karakostis.dimitris@gmail.com>
2018-02-09 09:47:27 +0100

    interface improvements

--- get_data_by_year.js
+++ get_data_by_year.js
@@ -2,7 +2,7 @@ function get_data_by_year(selected_year, country_iso3) {
 
     $.ajax({
         url:  "https://geonode.wfp.org/api/layers/?keywords__slug__in=ica_" + selected_year + "&limit=30&offset=0&regions__code__in=" + country_iso3,
-        type: "POST",
+        type: "GET",
         dataType: 'jsonp',
         xhrFields: {
             withCredentials: true
@@ -19,7 +19,7 @@ function get_data_by_year(selected_year, country_iso3) {
 
     $.ajax({
         url:  "https://geonode.wfp.org/trainings/api/v2.4/staticmaps/?keywords__slug__in=ica_" + selected_year + "&limit=30&offset=0&regions__code__in=" + country_iso3,
-        type: "POST",
+        type: "GET",
         dataType: 'jsonp',
         // This is the important part
         xhrFields: {
@@ -38,7 +38,7 @@ function get_data_by_year(selected_year, country_iso3) {
 
     $.ajax({
         url:  "https://geonode.wfp.org/api/documents/?keywords__slug__in=ica_" + selected_year + "&limit=30&offset=0&regions__code__in=" + country_iso3,
-        type: "POST",
+        type: "GET",
         dataType: 'jsonp',
         // This is the important part
         xhrFields: {
@@ -47,6 +47,25 @@ function get_data_by_year(selected_year, country_iso3) {
         success: function (response) {
             if(typeof(Storage)!=="undefined") {
                 sessionStorage['response_document'] = JSON.stringify(response);;
+                //sessionStorage.response_document= JSON.stringify(response);
+            }
+        },
+        error: function (xhr, status) {
+            // handle errors
+        }
+    });
+
+    $.ajax({
+        url:  "https://geonode.wfp.org/api/maps/?keywords__slug__in=ica_" + selected_year + "&limit=10&offset=0&regions__code__in=" + country_iso3,
+        type: "GET",
+        dataType: 'jsonp',
+        // This is the important part
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (response) {
+            if(typeof(Storage)!=="undefined") {
+                sessionStorage['response_map'] = JSON.stringify(response);
                 //sessionStorage.response_document= JSON.stringify(response);
             }
         },
